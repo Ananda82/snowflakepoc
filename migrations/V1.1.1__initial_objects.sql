9ece829089795a5de324d0596b87531ad5ebbc4a
Ananda82 <89700990+Ananda82@users.noreply.github.com>
2021-10-30 21:02:21 +0530

    Update V1.1.1__initial_objects.sql
    Table altered.

--- migrations/V1.1.1__initial_objects.sql
+++ migrations/V1.1.1__initial_objects.sql
@@ -1,10 +1,12 @@
 CREATE SCHEMA actor;
-CREATE TABLE actor (
+ALTER TABLE actor (
   actor_id INTEGER,
   first_name VARCHAR NOT NULL,
   middle_name VARCHAR NOT NULL,
   last_name VARCHAR NOT NULL,
+  address VARCHAR,
   last_update TIMESTAMP,
   first_update TIMESTAMP,
   PRIMARY KEY  (actor_id)
 );
+
