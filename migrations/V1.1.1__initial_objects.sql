e2c5efec1c8ef8a009e19ce72eddf65fe5357e36
EC2 Default User <ec2-user@ip-172-31-36-152.ap-south-1.compute.internal>
2021-10-30 11:06:03 +0000

    Update SQL File

--- migrations/V1.1.1__initial_objects.sql
+++ migrations/V1.1.1__initial_objects.sql
@@ -8,17 +8,4 @@ CREATE SCHEMA user_details;
 --
 -- Table structure for table `user_details`
 --
-
--- CREATE TABLE test (blob_col BLOB, INDEX(blob_col(10)));
-
-CREATE TABLE user_details (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) DEFAULT NULL,
-  `first_name` varchar(50) DEFAULT NULL,
-  `last_name` varchar(50) DEFAULT NULL,
-  `gender` varchar(10) DEFAULT NULL,
-  `password` varchar(50) DEFAULT NULL,
-  `status` tinyint(10) DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
-);
-
+CREATE TABLE user_details (blob_col VARCHAR, Name VARCHAR);
