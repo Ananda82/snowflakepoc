16c3b1dbd026813385c6738281effced2fcfbf8a
EC2 Default User <ec2-user@ip-172-31-36-152.ap-south-1.compute.internal>
2021-10-30 11:42:04 +0000

    Update SQL File

--- migrations/V1.1.1__initial_objects.sql
+++ migrations/V1.1.1__initial_objects.sql
@@ -2,6 +2,7 @@ CREATE SCHEMA actor;
 CREATE TABLE actor (
   actor_id INTEGER,
   first_name VARCHAR NOT NULL,
+  middle_name VARCHAR NOT NULL,
   last_name VARCHAR NOT NULL,
   last_update TIMESTAMP,
   first_update TIMESTAMP,
