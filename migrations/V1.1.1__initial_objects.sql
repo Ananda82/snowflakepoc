9623e08380e2a139b3c7c340a504f31261170e69
EC2 Default User <ec2-user@ip-172-31-36-152.ap-south-1.compute.internal>
2021-10-30 11:19:26 +0000

    Update SQL File

--- migrations/V1.1.1__initial_objects.sql
+++ migrations/V1.1.1__initial_objects.sql
@@ -10,10 +10,9 @@ CREATE SCHEMA actor;
 --
 --CREATE TABLE user_details (blob_col VARCHAR, Name VARCHAR);
 CREATE TABLE actor (
-  actor_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(45) NOT NULL,
-  last_name VARCHAR(45) NOT NULL,
-  last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  actor_id INTEGER,
+  first_name VARCHAR,
+  last_name VARCHAR,
+  last_update TIMESTAMP,
   PRIMARY KEY  (actor_id),
-  KEY idx_actor_last_name (last_name)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
